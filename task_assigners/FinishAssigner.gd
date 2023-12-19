b5751ff578286f1e41d86fdc1398187d63c54a1c
speak <speakk@gmail.com>
2023-12-19 13:31:00 +0200

    Clean up (aka remove) Beehave based task assigner stuff

--- task_assigners/FinishAssigner.gd
+++
@@ -1,9 +0,0 @@
-extends ActionLeaf
-
-var assigner: Variant
-
-@warning_ignore("untyped_declaration")
-func tick(actor: Object, blackboard: Blackboard):
-	print("Firing assigner finished??")
-	Events.task_assigner_finished.emit(assigner)
-	return SUCCESS
