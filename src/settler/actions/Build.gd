d69a50c76cf83db14383996bb18939ea460098c4
speak <speakk@gmail.com>
2024-02-10 13:50:05 +0200

    Zone rendering works in 3d now too

--- src/settler/actions/Build.gd
+++ src/settler/actions/Build.gd
@@ -11,3 +11,8 @@ func process_action(actor: Settler, delta: float) -> void:
 	build_target.increase_build_progress(actor.build_speed * delta)
 	if build_target.is_finished():
 		finished.emit(self)
+	
+	var target_position := build_target.global_position
+	var look_at_target := Vector3(target_position.x, actor.global_position.y, target_position.z)
+	if look_at_target != actor.global_position:
+		actor.look_at(look_at_target, Vector3.UP, true)
--- src/zones/zone_renderer.gd
+++ src/zones/zone_renderer.gd
@@ -1,9 +1,11 @@
-extends Node2D
+extends Node3D
+
+var ZONE_TILE_INDICATOR := preload("res://src/zones/zone_tile_indicator.tscn")
 
 var zones: Array[Zone]:
 	set(new_zones):
 		zones = new_zones
-		queue_redraw()
+		redraw()
 
 func _ready() -> void:
 	Events.zone_selected.connect(_zone_selected)
@@ -17,14 +19,18 @@ func _zone_menu_hidden() -> void:
 	zones = []
 	
 func _zone_updated(updated_zone: Zone) -> void:
+	pass
 	# Maybe check if updated_zone is within zones, but this is cheap so whatever
-	queue_redraw()
+	redraw()
 
-func _draw() -> void:
+func redraw() -> void:
+	for child in get_children():
+		child.queue_free()
+	
 	if zones:
 		for zone in zones:
 			for coordinate in zone.get_coordinates():
-				var tile_position := Globals.get_map().map_to_local(coordinate)
-				var rect := Rect2(tile_position - Vector2(MainMap.CELL_SIZE) / 2, MainMap.CELL_SIZE)
-				draw_rect(rect, Color(0.5, 1.0, 0.1, 0.2), true, 1)
-	
+				var tile_position := Globals.get_map().grid.map_to_local(Globals.extend_vec2i(coordinate))
+				var zone_tile_indicator := ZONE_TILE_INDICATOR.instantiate()
+				add_child(zone_tile_indicator)
+				zone_tile_indicator.global_position = Vector3(tile_position.x, 0, tile_position.z)
