19ce08bb7480cff143a695130cf5b5d8a7e4dba6
speak <speakk@gmail.com>
2024-02-14 16:35:33 +0200

    Remove obsolete growth_requirement files

--- src/plants/growth_requirement/GrowthRequirement.gd
+++
@@ -1,8 +0,0 @@
-class_name GrowthRequirement extends Resource
-
-enum Id {
-	Water, Fertilizer, Clay
-}
-
-@export var id: Id
-@export var display_name: String
--- src/plants/growth_requirement/growth_requirement_amount.gd
+++
@@ -1,4 +0,0 @@
-class_name GrowthRequirementAmount extends Resource
-
-@export var growth_requirement_id: GrowthRequirement.Id
-@export var amount: float
