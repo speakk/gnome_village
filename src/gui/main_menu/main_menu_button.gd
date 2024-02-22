6d09d98aa92efa6996ca3b23c6d9cd6c40092dcc
speak <speakk@gmail.com>
2024-02-23 00:11:00 +0200

    Main menu button wiggle

--- src/gui/main_menu/main_menu_button.gd
+++ src/gui/main_menu/main_menu_button.gd
@@ -8,30 +8,34 @@ var hovered := false
 
 @export var text: String:
 	set(new_value):
-		$Label3D.text = new_value
+		%Label3D.text = new_value
 		text = new_value
 
 
 func _ready() -> void:
-	$button/Cube.material_override = $button/Cube.get_active_material(0).duplicate()
-	$button/Cube.get_active_material(0).albedo_color = original_color
+	%button/Cube.material_override = %button/Cube.get_active_material(0).duplicate()
+	%button/Cube.get_active_material(0).albedo_color = original_color
 
 func on_hover() -> void:
 	hovered = true
 
 
 func _on_area_3d_input_event(camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	print("EVENT", event)
+	#print("EVENT", event)
+	pass
 
 
 func _on_area_3d_mouse_entered() -> void:
 	hovered = true
-	var material: StandardMaterial3D = $button/Cube.get_active_material(0)
+	var material: StandardMaterial3D = %button/Cube.get_active_material(0)
 	material.albedo_color = hover_color
 	$ClickSoundPlayer.play()
+	
+	await create_tween().tween_property($Node3D, "rotation_degrees", Vector3(0, -10, 0), 0.1).finished
+	create_tween().tween_property($Node3D, "rotation_degrees", Vector3(0, 0, 0), 0.5).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
 
 
 func _on_area_3d_mouse_exited() -> void:
 	hovered = false
-	var material: StandardMaterial3D = $button/Cube.get_active_material(0)
+	var material: StandardMaterial3D = %button/Cube.get_active_material(0)
 	material.albedo_color = original_color
