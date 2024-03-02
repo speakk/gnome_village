d4dfc3fdb9bc6fb9a5c85a035d94e1de15d2e6bf
speak <speakk@gmail.com>
2024-03-02 14:29:27 +0200

    Get rid of the old plants dir

--- src/plants/plant_data/Plant.gd
+++
@@ -1,9 +0,0 @@
-class_name Plant extends Resource
-
-@export var display_name: String
-
-## How long does it take to progress to next growth stage (in seconds)
-@export var growth_stage_length: float = 2.0
-@export var growth_stages: Array[GrowthStage]
-
-@export var growth_requirements: Array[ItemRequirement]
