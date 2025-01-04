39edbd4b903a7e1843ab1cd38d657adaeebaa329
Jesse Viikari <ext.jesse.viikari@eezy.fi>
2025-01-04 23:49:35 +0200

    Rudimentary day and night cycle

--- src/features/camera.rs
+++ src/features/camera.rs
@@ -3,26 +3,17 @@ use crate::features::movement::{Acceleration, Friction, Velocity};
 use crate::features::position::{PreviousWorldPosition, WorldPosition};
 use bevy::app::RunFixedMainLoopSystem::BeforeFixedMainLoop;
 use bevy::app::{App, Plugin, RunFixedMainLoop, Startup};
-use bevy::asset::LoadState;
-use bevy::core_pipeline::Skybox;
 use bevy::ecs::prelude::*;
-use bevy::image::{ImageSampler, ImageSamplerDescriptor};
 use bevy::math::{Vec2, Vec3};
 use bevy::pbr::ScreenSpaceAmbientOcclusion;
 use bevy::prelude::KeyCode::{KeyA, KeyD, KeyS, KeyW};
 use bevy::prelude::*;
 use bevy::render::camera::ScalingMode;
-use bevy::render::render_resource::{TextureViewDescriptor, TextureViewDimension};
 use bevy_atmosphere::plugin::{AtmosphereCamera, AtmospherePlugin};
 use leafwing_input_manager::prelude::*;
 
 pub struct CameraPlugin;
 
-#[derive(Resource)]
-pub struct SkyBoxMap {
-    pub image: Handle<Image>,
-    pub loaded: bool,
-}
 /// A vector representing the player's input, accumulated over all frames that ran
 /// since the last time the physics simulation was advanced.
 #[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut)]
@@ -32,8 +23,7 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(AtmospherePlugin)
             .add_systems(Startup, setup)
-            .add_systems(RunFixedMainLoop, handle_input.in_set(BeforeFixedMainLoop))
-            .add_systems(Update, reinterpret_cubemap);
+            .add_systems(RunFixedMainLoop, handle_input.in_set(BeforeFixedMainLoop));
     }
 }
 
@@ -44,12 +34,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         (CameraPanAction::Up, KeyW),
         (CameraPanAction::Down, KeyS),
     ]);
-
-    let sky_image_handle = asset_server.load::<Image>("skybox.png");
-    commands.insert_resource(SkyBoxMap {
-        image: sky_image_handle.clone(),
-        loaded: false,
-    });
 
     // camera
     commands.spawn((
@@ -80,32 +64,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         AccumulatedInput::default(),
         Friction(0.5),
     ));
-}
-
-pub fn reinterpret_cubemap(
-    asset_server: Res<AssetServer>,
-    mut images: ResMut<Assets<Image>>,
-    mut cubemap: ResMut<SkyBoxMap>,
-    mut skyboxes: Query<&mut Skybox>,
-) {
-    if !cubemap.loaded && matches!(asset_server.load_state(&cubemap.image), LoadState::Loaded) {
-        cubemap.loaded = true;
-        let image = images.get_mut(&cubemap.image).unwrap();
-
-        if image.texture_descriptor.array_layer_count() == 1 {
-            //6
-            image.reinterpret_stacked_2d_as_array(image.height() / image.width());
-            image.sampler = ImageSampler::Descriptor(ImageSamplerDescriptor::nearest());
-            image.texture_view_descriptor = Some(TextureViewDescriptor {
-                dimension: Some(TextureViewDimension::Cube),
-                ..Default::default()
-            });
-        }
-        //set all skybox images to the new array texture
-        for mut skybox in &mut skyboxes {
-            skybox.image = cubemap.image.clone();
-        }
-    }
 }
 
 fn handle_input(
