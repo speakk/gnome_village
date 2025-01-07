f2fdcfb3659b062d81c03a9d1024ad6fdf304a43
Jesse Viikari <ext.jesse.viikari@eezy.fi>
2025-01-07 14:40:41 +0200

    Separate ui definitions via manifest, add font loading in button, colour scheme beginnings

---
+++ assets/ui_templates/colour_scheme.cob
@@ -0,0 +1,6 @@
+#defs
+
+$text_colour_light = Hsla{hue:45 saturation:1.0 lightness:0.6 alpha:1.0}
+$text_colour_dark = Hsla{hue:45 saturation:1.0 lightness:0.2 alpha:1.0}
+
+$primary  = Hsla{hue:45 saturation:1.0 lightness:0.2 alpha:1.0}
