92cf357184bbe4e72e219118ab03f522770ac2b7
speak <speakk@gmail.com>
2025-01-27 10:30:32 +0200

    Move GoTo action to its own file

---
+++ src/features/ai/actions/go_to.rs
@@ -0,0 +1,51 @@
+use bevy::math::{IVec2, Vec2};
+use bevy::prelude::{Commands, Entity, In, Query, Res, With};
+use bevior_tree::prelude::{delegate_node, TaskBridge, TaskEvent, TaskStatus};
+use bevior_tree::node::NodeResult;
+use crate::bundles::settler::Settler;
+use crate::features::ai::PathFollow;
+use crate::features::map::map_model::MapData;
+use crate::features::path_finding::{spawn_pathfinding_task, PathfindingTask, PathingGridResource};
+use crate::features::position::WorldPosition;
+
+#[delegate_node(delegate)]
+pub struct GoTo {
+    delegate: TaskBridge,
+}
+
+const TARGET_DISTANCE_THRESHOLD: f32 = 1.5;
+
+impl GoTo {
+    pub fn new(target_coordinate: IVec2) -> Self {
+        let checker = move |entity: In<Entity>, query: Query<(&WorldPosition, Option<&PathfindingTask>, Option<&PathFollow>), With<Settler>>| {
+            let (world_position, path_finding_task, path_follow) = query.get(entity.0).unwrap();
+            
+            if path_finding_task.is_some() || path_follow.is_some() {
+                let distance_to_target = world_position.0.distance(target_coordinate.as_vec2());
+
+                return if distance_to_target > TARGET_DISTANCE_THRESHOLD {
+                    TaskStatus::Running
+                } else {
+                    println!("Reached target, returning success");
+                    TaskStatus::Complete(NodeResult::Success)
+                }
+            }
+
+            println!("Didn't have path, returning failure");
+            return TaskStatus::Complete(NodeResult::Failure);
+        };
+
+        let task = TaskBridge::new(checker)
+            .on_event(TaskEvent::Enter, move |entity: In<Entity>,
+                                              mut commands: Commands,
+                                              map_data: Query<&MapData>,
+                                              pathing_grid: Res<PathingGridResource>,
+                                              world_position: Query<&WorldPosition>| {
+                println!("Ensure path entered, to {}", target_coordinate);
+                let target_position = WorldPosition(Vec2::new(target_coordinate.x as f32, target_coordinate.y as f32));
+                spawn_pathfinding_task(&mut commands, *entity, &pathing_grid, map_data.single(), *world_position.get(*entity).unwrap(), target_position);
+            });
+
+        Self { delegate: task }
+    }
+}
--- src/features/ai/trees/bring_resource.rs
+++ src/features/ai/trees/bring_resource.rs
@@ -1,14 +1,9 @@
-use crate::features::ai::{PathFollow, WorkingOnTask};
-use crate::features::tasks::task::{BringResourceData, DepositTarget, Task, TaskType};
-use bevior_tree::prelude::{delegate_node, ConditionalLoop, Sequence, TaskBridge};
+use crate::features::ai::actions::go_to::GoTo;
+use crate::features::ai::WorkingOnTask;
+use crate::features::tasks::task::{DepositTarget, Task, TaskType};
+use bevior_tree::prelude::Sequence;
 use bevior_tree::BehaviorTreeBundle;
-use bevior_tree::node::NodeResult;
-use bevior_tree::task::{TaskEvent, TaskStatus};
 use bevy::prelude::*;
-use crate::bundles::settler::Settler;
-use crate::features::map::map_model::MapData;
-use crate::features::path_finding::{spawn_pathfinding_task, PathfindingTask, PathingGridResource};
-use crate::features::position::WorldPosition;
 
 pub fn create_bring_resource_tree(
     work_started_query: Query<(&WorkingOnTask, Entity), Added<WorkingOnTask>>,
@@ -42,69 +37,3 @@ pub fn create_bring_resource_tree(
         }
     }
 }
-
-#[delegate_node(delegate)]
-struct GoTo {
-    delegate: TaskBridge,
-}
-
-const TARGET_DISTANCE_THRESHOLD: f32 = 1.5;
-
-impl GoTo {
-    pub fn new(target_coordinate: IVec2) -> Self {
-        let checker = move |entity: In<Entity>, query: Query<(&WorldPosition, Option<&PathfindingTask>, Option<&PathFollow>), With<Settler>>| {
-            let (world_position, path_finding_task, path_follow) = query.get(entity.0).unwrap();
-            
-            if path_finding_task.is_some() || path_follow.is_some() {
-                let distance_to_target = world_position.0.distance(target_coordinate.as_vec2());
-
-                return if distance_to_target > TARGET_DISTANCE_THRESHOLD {
-                    TaskStatus::Running
-                } else {
-                    println!("Reached target, returning success");
-                    TaskStatus::Complete(NodeResult::Success)
-                }
-            }
-
-            println!("Didn't have path, returning failure");
-            return TaskStatus::Complete(NodeResult::Failure);
-        };
-
-        let task = TaskBridge::new(checker)
-            .on_event(TaskEvent::Enter, move |entity: In<Entity>,
-                                              mut commands: Commands,
-                                              map_data: Query<&MapData>,
-                                              pathing_grid: Res<PathingGridResource>,
-                                              world_position: Query<&WorldPosition>| {
-                println!("Ensure path entered, to {}", target_coordinate);
-                let target_position = WorldPosition(Vec2::new(target_coordinate.x as f32, target_coordinate.y as f32));
-                spawn_pathfinding_task(&mut commands, *entity, &pathing_grid, map_data.single(), *world_position.get(*entity).unwrap(), target_position);
-            });
-
-        Self { delegate: task }
-    }
-}
-
-// use bevior_tree::prelude::{delegate_node, TaskBridge};
-// use bevy::prelude::*;
-// use crate::features::tasks::task::{BringResourceData, TaskType};
-//
-// // Task to wait until player get near.
-// // Task trait is available for making your task, delegating core methods to TaskImpl.
-// #[delegate_node(delegate)]
-// struct BringResourceTree {
-//     delegate: TaskBridge,
-// }
-//
-// impl BringResourceTree {
-//     pub fn new(bring_resource_data: BringResourceData) -> Self {
-//         let checker = move |In(entity): In<Entity>, param: Query<&Transform>| {
-//             let distance = get_distance(entity, target, param);
-//             // Return `Failure` if target is out of range.
-//             match distance <= range {
-//                 true => TaskStatus::Running,
-//                 false => TaskStatus::Complete(NodeResult::Failure),
-//             }
-//         };
-//     }
-// }
