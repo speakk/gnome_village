8651a8506a262dbaf2b08439b3561b0f506680c1
speak <speakk@gmail.com>
2025-01-30 22:38:59 +0200

    Make models face their velocity direction

--- src/features/misc_components.rs
+++ src/features/misc_components.rs
@@ -2,15 +2,17 @@ pub(crate) mod gltf_asset;
 pub mod simple_mesh;
 mod simple_mesh_view;
 
+use std::f32::consts::PI;
 use crate::features::misc_components::gltf_asset::{GltfAsset, GltfAssetPlugin};
 use crate::features::misc_components::simple_mesh::{SimpleMesh, SimpleMeshHandles};
 use crate::features::misc_components::simple_mesh_view::{on_add_blueprint, on_remove_blueprint};
-use crate::features::position::WorldPosition;
+use crate::features::position::{PreviousWorldPosition, WorldPosition};
 use bevy::prelude::*;
 use bevy::utils::HashMap;
 use moonshine_core::prelude::Save;
 use moonshine_object::{Object, ObjectInstance};
 use moonshine_view::{BuildView, RegisterView, ViewCommands, Viewable};
+use crate::features::movement::Velocity;
 
 pub struct MiscComponentsPlugin;
 
@@ -24,9 +26,13 @@ impl Plugin for MiscComponentsPlugin {
                 PostUpdate,
                 (
                     viewable_moved::<SimpleMesh>,
+                    update_viewable_rotation::<SimpleMesh>,
                     viewable_moved::<GltfAsset>,
+                    update_viewable_rotation::<GltfAsset>,
                     viewable_moved::<LightSource>,
+                    update_viewable_rotation::<LightSource>,
                 ),
+
             )
             .add_viewable::<SimpleMesh>()
             .add_viewable::<LightSource>();
@@ -91,6 +97,22 @@ pub fn viewable_moved<T>(
     for (position, model) in query.iter() {
         let view = model.view();
         let mut transform = transform.get_mut(view.entity()).unwrap();
-        *transform = Transform::from_xyz(position.x, 0.5, position.y);
+        transform.translation = Vec3::new(position.x, 0.5, position.y);
+    }
+}
+
+pub fn update_viewable_rotation<T>(
+    query: Query<(&Viewable<T>, &Velocity)>,
+    mut transform: Query<&mut Transform>,
+    time: Res<Time>
+) where T: Component {
+    for (model, velocity) in query.iter() {
+        let view = model.view();
+        let mut transform = transform.get_mut(view.entity()).unwrap();
+        if velocity.0.length() > 0.5 {
+            let mut target_transform = *transform;
+            target_transform.look_to(-Vec3::new(velocity.0.x, 0.0, velocity.0.y), Vec3::Y);
+            transform.rotation.smooth_nudge(&target_transform.rotation, 5.0, time.delta_secs());
+        }
     }
 }
