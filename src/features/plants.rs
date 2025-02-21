732a1d75fd2b46ad14a2f07243f1193e1789215c
speak <speakk@gmail.com>
2025-02-21 13:05:41 +0200

    Run plant growth at less frequent schedule in preparation for slightly expensive growth requirement check

--- src/features/plants.rs
+++ src/features/plants.rs
@@ -1,8 +1,10 @@
+use std::time::Duration;
 use crate::features::misc_components::gltf_asset::GltfData;
 use crate::features::misc_components::ItemAmount;
 use bevy::app::{App, Plugin};
 use bevy::prelude::*;
 use bevy::prelude::{Component, Reflect};
+use bevy::time::common_conditions::on_timer;
 use rand::Rng;
 
 pub struct PlantsPlugin;
@@ -11,7 +13,7 @@ impl Plugin for PlantsPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<PlantStageAdvanced>()
             .add_systems(Update, initialize_plant)
-            .add_systems(FixedUpdate, update_growth_process)
+            .add_systems(Update, update_growth_process.run_if(on_timer(Duration::from_millis(100))))
             .add_observer(update_gltf_based_on_growth_stage);
     }
 }
@@ -56,16 +58,26 @@ pub fn initialize_plant(mut commands: Commands, query: Query<Entity, Added<Plant
 
 pub fn update_growth_process(
     mut query: Query<(Entity, &mut Plant)>,
-    time: Res<Time<Fixed>>,
+    time: Res<Time>,
+    mut previous_run: Local<f32>,
     mut commands: Commands,
 ) {
+    let current_time = time.elapsed_secs();
+    let delta = current_time - *previous_run;
+    *previous_run = current_time;
+
+
     for (entity, mut plant) in query.iter_mut() {
         if plant.finished_growing {
             continue;
         }
 
+        // if !check_growth_requirements(&plant.growth_requirements) {
+        //     continue;
+        // }
+
         plant.current_stage_growth_process +=
-            plant.growth_speed * plant.random_growth_multiplier * time.delta_secs();
+            plant.growth_speed * plant.random_growth_multiplier * delta;
         if plant.current_stage_growth_process >= 1.0 {
             plant.current_stage_growth_process = 0.0;
             plant.current_growth_stage += 1;
