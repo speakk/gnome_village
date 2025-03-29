67b689f8ba360ee30b1b0879626ee47bf6c00c36
speak <speakk@gmail.com>
2025-03-29 12:24:13 +0200

    Sun rotates roughly according to time_of_day. Separate sun systems out a bit more. Still WIP

--- src/features/sun_light.rs
+++ src/features/sun_light.rs
@@ -1,15 +1,15 @@
 use crate::features::states::AppState;
 use bevy::app::App;
+use bevy::color::palettes::basic::NAVY;
 use bevy::color::palettes::css::ORANGE_RED;
 use bevy::color::palettes::tailwind::SKY_200;
 use bevy::pbr::light_consts::lux::AMBIENT_DAYLIGHT;
 use bevy::pbr::{AmbientLight, CascadeShadowConfigBuilder, DirectionalLight};
 use bevy::prelude::*;
+use bevy::window::WindowResized;
 use bevy_atmosphere::model::AtmosphereModel;
 use bevy_atmosphere::prelude::{AtmosphereMut, Nishita};
 use std::f32::consts::PI;
-use bevy::color::palettes::basic::NAVY;
-use bevy::window::WindowResized;
 
 pub struct SunLightPlugin;
 
@@ -29,10 +29,20 @@ pub struct PlanetOrigin;
 struct CycleTimer(Timer);
 
 #[derive(Resource)]
-struct AtmosphereTimer(Timer);
+pub struct CurrentTimeOfDay {
+    pub total_time: f32,
+    /// 0.0 midnight, 0.5 noon
+    pub time_of_day: f32,
+}
 
-#[derive(Event)]
-pub struct DayTimeChanged(pub f32);
+impl Default for CurrentTimeOfDay {
+    fn default() -> Self {
+        CurrentTimeOfDay {
+            total_time: 0.0,
+            time_of_day: 0.3, // Let's start in the morning
+        }
+    }
+}
 
 impl Plugin for SunLightPlugin {
     fn build(&self, app: &mut App) {
@@ -45,10 +55,13 @@ impl Plugin for SunLightPlugin {
                 bevy::utils::Duration::from_millis(2000),
                 TimerMode::Repeating,
             )))
+            .insert_resource(CurrentTimeOfDay::default())
             .insert_resource(AtmosphereModel::default())
-            .add_event::<DayTimeChanged>()
-            .add_systems(Update, daylight_cycle.run_if(in_state(AppState::InGame)));
-
+            .add_systems(
+                Update,
+                (daylight_cycle, rotate_planet, rotate_atmosphere)
+                    .run_if(in_state(AppState::InGame)),
+            );
     }
 }
 
@@ -59,7 +72,11 @@ pub fn setup_lights(mut commands: Commands) {
     });
 
     commands
-        .spawn((PlanetOrigin, Transform::default(), Name::from("PlanetOrigin")))
+        .spawn((
+            PlanetOrigin,
+            Transform::default(),
+            Name::from("PlanetOrigin"),
+        ))
         .with_children(move |child_builder| {
             let maximum_shadow_distance = 100.0;
             let cascade_count = 1;
@@ -122,8 +139,29 @@ pub fn setup_lights(mut commands: Commands) {
 #[allow(clippy::too_many_arguments)]
 #[allow(clippy::type_complexity)]
 fn daylight_cycle(
-    mut commands: Commands,
-    mut atmosphere: AtmosphereMut<Nishita>,
+    mut timer: ResMut<CycleTimer>,
+    mut current_time_of_day: ResMut<CurrentTimeOfDay>,
+    time: Res<Time>,
+) {
+    timer.0.tick(time.delta());
+
+    // TODO: Figure out the math in this, this was partially straight from bevy_atmosphere example
+
+    let timer_scale_division = 6.0;
+    let t = time.elapsed_secs_wrapped() / timer_scale_division;
+
+    if timer.0.finished() {
+        const DAY_LENGTH: f32 = 30.0;
+        let time_of_day = t % DAY_LENGTH;
+        current_time_of_day.total_time = t;
+        current_time_of_day.time_of_day = time_of_day;
+    }
+}
+
+#[derive(Resource)]
+struct AtmosphereTimer(Timer);
+
+pub fn rotate_planet(
     mut sun_query: Query<
         (&mut Transform, &mut DirectionalLight, Entity),
         (With<Sun>, Without<Moon>),
@@ -133,63 +171,60 @@ fn daylight_cycle(
         (With<Moon>, Without<Sun>),
     >,
     mut planet_origin: Query<&mut Transform, (Without<Moon>, Without<Sun>, With<PlanetOrigin>)>,
-    mut visibility_query: Query<&mut Visibility>,
-    mut timer: ResMut<CycleTimer>,
-    mut atmosphere_timer: ResMut<AtmosphereTimer>,
-    mut day_time_changed: EventWriter<DayTimeChanged>,
-    time: Res<Time>,
+    current_time_of_day: Res<CurrentTimeOfDay>,
 ) {
-    timer.0.tick(time.delta());
-    atmosphere_timer.0.tick(time.delta());
-
-    // TODO: Figure out the math in this, this was partially straight from bevy_atmosphere example
-
-    let timer_scale_division = 6.0;
-    let t = time.elapsed_secs_wrapped() / timer_scale_division;
-
-    if atmosphere_timer.0.finished() {
-        atmosphere.sun_position = Vec3::new(0., t.sin(), t.cos());
+    if !current_time_of_day.is_changed() {
+        return;
     }
 
-    if timer.0.finished() {
-        day_time_changed.send(DayTimeChanged(t));
-        
-        if let Ok(mut transform) = planet_origin.get_single_mut() {
-            // transform.rotation = Quat::from_rotation_z(PI / 2.0);
-            transform.rotation = Quat::from_euler(EulerRot::YXZ, t, t, 0.0);
-            //transform.rotate_local_x(t)
-        }
+    let t = current_time_of_day.time_of_day;
 
-        if let Some((mut light_trans, mut directional, entity)) = sun_query.single_mut().into() {
-            light_trans.look_at(Vec3::ZERO, Vec3::Y);
-            //light_trans.rotation = Quat::from_rotation_x(-t);
-            // let illuminance = t.sin().max(0.0).powf(2.0) * AMBIENT_DAYLIGHT;
-            // // TODO: Base this on rotation
-            // if illuminance < 10.0 {
-            //     if visibility_query.get_mut(entity).is_ok() {
-            //         commands.entity(entity).remove::<Visibility>();
-            //     }
-            // } else if let Err(_visibility) = visibility_query.get_mut(entity) {
-            //     commands.entity(entity).insert(Visibility::Visible);
-            // }
-            // directional.illuminance = illuminance;1
-        }
+    if let Ok(mut transform) = planet_origin.get_single_mut() {
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, t * PI / 4.0, t * PI, 0.0);
+    }
 
-        if let Some((mut light_trans, mut directional, entity)) = moon_query.single_mut().into() {
-            // let moon_t = -t - PI;
-            // //light_trans.rotation = Quat::from_rotation_x(moon_t);
-            // let illuminance = (-moon_t).sin().max(0.2).powf(2.0) * AMBIENT_DAYLIGHT * 0.5;
-            //
-            // // TODO: Base this on rotation
-            // if illuminance < 201.0 {
-            //     if visibility_query.get_mut(entity).is_ok() {
-            //         commands.entity(entity).remove::<Visibility>();
-            //     }
-            // } else if let Err(_visibility) = visibility_query.get_mut(entity) {
-            //     commands.entity(entity).insert(Visibility::Visible);
-            // }
-            //
-            // directional.illuminance = illuminance;
-        }
+    if let Some((mut light_trans, mut directional, entity)) = sun_query.single_mut().into() {
+        light_trans.look_at(Vec3::ZERO, Vec3::Y);
+        //light_trans.rotation = Quat::from_rotation_x(-t);
+        // let illuminance = t.sin().max(0.0).powf(2.0) * AMBIENT_DAYLIGHT;
+        // // TODO: Base this on rotation
+        // if illuminance < 10.0 {
+        //     if visibility_query.get_mut(entity).is_ok() {
+        //         commands.entity(entity).remove::<Visibility>();
+        //     }
+        // } else if let Err(_visibility) = visibility_query.get_mut(entity) {
+        //     commands.entity(entity).insert(Visibility::Visible);
+        // }
+        // directional.illuminance = illuminance;1
+    }
+
+    if let Some((mut light_trans, mut directional, entity)) = moon_query.single_mut().into() {
+        // let moon_t = -t - PI;
+        // //light_trans.rotation = Quat::from_rotation_x(moon_t);
+        // let illuminance = (-moon_t).sin().max(0.2).powf(2.0) * AMBIENT_DAYLIGHT * 0.5;
+        //
+        // // TODO: Base this on rotation
+        // if illuminance < 201.0 {
+        //     if visibility_query.get_mut(entity).is_ok() {
+        //         commands.entity(entity).remove::<Visibility>();
+        //     }
+        // } else if let Err(_visibility) = visibility_query.get_mut(entity) {
+        //     commands.entity(entity).insert(Visibility::Visible);
+        // }
+        //
+        // directional.illuminance = illuminance;
+    }
+}
+
+pub fn rotate_atmosphere(
+    mut atmosphere: AtmosphereMut<Nishita>,
+    mut atmosphere_timer: ResMut<AtmosphereTimer>,
+    current_time_of_day: Res<CurrentTimeOfDay>,
+    time: Res<Time>,
+) {
+    atmosphere_timer.0.tick(time.delta());
+    if atmosphere_timer.0.finished() {
+        let t = current_time_of_day.time_of_day;
+        atmosphere.sun_position = Vec3::new(0., t.sin(), t.cos());
     }
 }
--- src/ui/day_cycle_indicator.rs
+++ src/ui/day_cycle_indicator.rs
@@ -1,9 +1,11 @@
 use bevy::color::palettes::basic::NAVY;
-use bevy::color::palettes::css::RED;
+use bevy::color::palettes::css::{RED, SIENNA};
 use bevy::math::Vec2;
 use bevy::prelude::*;
 use bevy::render::mesh::CircleMeshBuilder;
 use bevy::window::WindowResized;
+use std::f32::consts::PI;
+use crate::features::sun_light::CurrentTimeOfDay;
 
 pub(super) struct DayCycleIndicatorPlugin;
 
@@ -11,9 +13,21 @@ impl Plugin for DayCycleIndicatorPlugin {
     fn build(&self, app: &mut App) {
         app.init_gizmo_group::<MyGizmos>()
             .add_systems(Startup, setup)
-            .add_systems(Update, draw_gizmos);
+            .init_resource::<CurrentPosition>()
+            .add_systems(
+                Update,
+                (
+                    update_current_position,
+                    //draw_gizmos,
+                    position_indicator,
+                    rotate_indicator,
+                ),
+            );
     }
 }
+
+static RADIUS: f32 = 50.0;
+static PADDING: f32 = 7.0;
 
 // We can create our own gizmo config group!
 #[derive(Default, Reflect, GizmoConfigGroup)]
@@ -22,51 +36,90 @@ struct MyGizmos {}
 #[derive(Component, Default)]
 struct DayCycleCircle;
 
+#[derive(Component, Default)]
+struct DayCycleCircleBorder;
+
+#[derive(Resource, Default, Deref)]
+struct CurrentPosition(Vec2);
+
 fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut commands: Commands,
-    //mut materials: ResMut<Assets<ColorMaterial>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
     let texture_handle = asset_server.load("daynight_cycle.png");
-    
-    let handle = meshes.add(CircleMeshBuilder::new(20.0, 30));
+
+    let handle = meshes.add(CircleMeshBuilder::new(RADIUS, 30));
     commands.spawn((
         Mesh2d(handle),
         DayCycleCircle,
         MeshMaterial2d(materials.add(texture_handle)),
-        //MeshMaterial2d(materials.add(ColorMaterial::from_color(RED))),
+    ));
+
+    let handle = meshes.add(CircleMeshBuilder::new(RADIUS + PADDING, 30));
+    commands.spawn((
+        Mesh2d(handle),
+        DayCycleCircleBorder,
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(SIENNA))),
     ));
 }
+//
+// fn draw_gizmos(
+//     mut gizmos: Gizmos<MyGizmos>,
+//     mut resize_reader: EventReader<WindowResized>,
+//     mut current_size: Local<Vec2>,
+//     current_position: Res<CurrentPosition>
+// ) {
+//     for event in resize_reader.read() {
+//         current_size.x = event.width;
+//         current_size.y = event.height;
+//         println!("new size: {:?}", current_size);
+//     }
+//
+//     const PADDING: f32 = 5.0;
+//
+//     gizmos
+//         .circle_2d(current_position.0, RADIUS + PADDING, NAVY)
+//         .resolution(64);
+// }
 
-fn draw_gizmos(
-    mut gizmos: Gizmos<MyGizmos>,
-    mut resize_reader: EventReader<WindowResized>,
-    mut current_size: Local<Vec2>,
-    mut commands: Commands,
-    mut query: Query<(Entity, Option<&mut Transform>), With<DayCycleCircle>>,
+fn rotate_indicator(
+    mut query: Query<&mut Transform, With<DayCycleCircle>>,
+    current_time_of_day: Res<CurrentTimeOfDay>
 ) {
-    for event in resize_reader.read() {
-        current_size.x = event.width;
-        current_size.y = event.height;
-        println!("new size: {:?}", current_size);
-    }
-
-    let radius = 20.0;
-    let gizmo_position = Vec2::new(
-        current_size.x / 2.0 - radius * 2.0,
-        -current_size.y / 2.0 + radius * 2.0,
-    );
-    // 
-    // gizmos
-    //     .circle_2d(gizmo_position, radius, NAVY)
-    //     .resolution(64);
-
-    for (entity, transform) in query.iter_mut() {
-        if let Some(mut transform) = transform {
-            transform.translation = gizmo_position.extend(0.0);
+    if current_time_of_day.is_changed() {
+        for mut transform in query.iter_mut() {
+            transform.rotation = Quat::from_rotation_z(current_time_of_day.time_of_day * PI);
         }
     }
 }
 
+fn update_current_position(
+    mut resize_reader: EventReader<WindowResized>,
+    mut current_position: ResMut<CurrentPosition>,
+) {
+    for event in resize_reader.read() {
+        let new_position = Vec2::new(event.width / 2.0 - RADIUS * 2.0, -event.height / 2.0);
+        current_position.0 = new_position;
+    }
+}
+
+fn position_indicator(
+    current_position: Res<CurrentPosition>,
+    mut query: Query<
+        (
+            &mut Transform,
+            Option<&DayCycleCircle>,
+            Option<&DayCycleCircleBorder>,
+        ),
+        Or<(With<DayCycleCircle>, With<DayCycleCircleBorder>)>,
+    >,
+) {
+    if current_position.is_changed() {
+        for (mut transform, circle, border) in query.iter_mut() {
+            let z_index = if circle.is_some() { 1.0 } else { 0.0 };
+            transform.translation = current_position.extend(z_index);
+        }
+    }
+}
