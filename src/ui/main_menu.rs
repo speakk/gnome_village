81a24bade69292ebf9460a078a82d9192ff7d02d
speak <speakk@gmail.com>
2025-04-08 17:48:25 +0300

    Create_button function

--- src/ui/main_menu.rs
+++ src/ui/main_menu.rs
@@ -1,6 +1,7 @@
 use crate::features::states::AppState;
-use crate::features::states::AppState::{MainMenu, Preload};
+use crate::features::states::AppState::MainMenu;
 use bevy::color::palettes::basic::RED;
+use bevy::ecs::system::{IntoObserverSystem, ObserverSystem};
 use bevy::prelude::*;
 
 pub struct MainMenuPlugin;
@@ -13,9 +14,20 @@ impl Plugin for MainMenuPlugin {
     }
 }
 
-const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+/*
+<palette>
+  <color name="Sage" hex="a3a380" r="163" g="163" b="128" />
+  <color name="Vanilla" hex="d6ce93" r="214" g="206" b="147" />
+  <color name="Beige" hex="efebce" r="239" g="235" b="206" />
+  <color name="Buff" hex="d8a48f" r="216" g="164" b="143" />
+  <color name="Old rose" hex="bb8588" r="187" g="133" b="136" />
+</palette>
+ */
+
+const NORMAL_BUTTON: Color = Color::srgb(0.31, 0.25, 0.25);
+const HOVERED_BUTTON: Color = Color::srgb(0.36, 0.29, 0.25);
 const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+const BORDER: Color = Color::srgb(0.35, 0.75, 0.35);
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
@@ -39,41 +51,50 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             StateScoped(MainMenu),
         ))
         .with_children(|parent| {
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .observe(
-                    |mut trigger: Trigger<Pointer<Click>>,
-                     mut next_state: ResMut<NextState<AppState>>| {
-                        //focus.0 = Some(trigger.target());
-                        next_state.set(Preload);
+            create_button(
+                parent,
+                &asset_server,
+                IntoObserverSystem::into_system(
+                    move |mut trigger: Trigger<Pointer<Click>>,
+                          mut next_state: ResMut<NextState<AppState>>| {
+                        next_state.set(AppState::Preload);
                         trigger.propagate(false);
                     },
-                )
-                .with_child((
-                    Text::new("New Game"),
-                    TextFont {
-                        font: asset_server.load("fonts/ThaleahFat.ttf"),
-                        font_size: 23.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
+                ),
+            );
         });
+}
+
+fn create_button(
+    parent: &mut ChildBuilder,
+    asset_server: &Res<AssetServer>,
+    observe_logic: impl ObserverSystem<Pointer<Click>, (), ()> + 'static,
+) {
+    parent
+        .spawn((
+            Button,
+            Node {
+                width: Val::Px(150.0),
+                height: Val::Px(65.0),
+                border: UiRect::all(Val::Px(2.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BorderColor(BORDER),
+            BorderRadius::all(Val::Px(5.0)),
+            BackgroundColor(NORMAL_BUTTON),
+        ))
+        .with_child((
+            Text::new("New Game"),
+            TextFont {
+                font: asset_server.load("fonts/ThaleahFat.ttf"),
+                font_size: 23.0,
+                ..default()
+            },
+            TextColor(Color::srgb(0.9, 0.9, 0.9)),
+        ))
+        .observe(observe_logic);
 }
 
 fn button_system(
@@ -92,17 +113,17 @@ fn button_system(
         let mut text = text_query.get_mut(children[0]).unwrap();
         match *interaction {
             Interaction::Pressed => {
-                **text = "Press".to_string();
+                //**text = "Press".to_string();
                 *color = PRESSED_BUTTON.into();
                 border_color.0 = RED.into();
             }
             Interaction::Hovered => {
-                **text = "Hover".to_string();
+                //**text = "Hover".to_string();
                 *color = HOVERED_BUTTON.into();
                 border_color.0 = Color::WHITE;
             }
             Interaction::None => {
-                **text = "Button".to_string();
+                //**text = "Button".to_string();
                 *color = NORMAL_BUTTON.into();
                 border_color.0 = Color::BLACK;
             }
